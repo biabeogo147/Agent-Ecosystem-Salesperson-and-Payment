bd876ce68382f0b9ced05a90c040354364c3a711
biabeogo147 <biabeogo147@gmail.com>
2025-10-17 23:07:24 +0700

    refactor

--- src/tests/test_es_db.py
+++ src/tests/test_es_db.py
@@ -1,10 +1,9 @@
 import time
-
 from config import ELASTIC_INDEX
-from data.elasic_search.elastic_search_index import create_products_index
-from data.elasic_search.sync_db_to_es import sync_products_to_elastic
 from data.es_connection import ElasticConnection
 from data.operations.product_ops import find_products_list_by_substring
+from data.elasic_search.elastic_search_index import create_products_index
+from data.elasic_search.sync_db_to_es import sync_products_to_elastic
 
 
 def test_full_pipeline_cleanup():
